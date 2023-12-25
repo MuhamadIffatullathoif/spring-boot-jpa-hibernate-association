e56e8f1f548c5dfa5f05f0645b33eff763c0ad03
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-25 07:35:08 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springboot/association/SpringBootHibernateJpaAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springboot.association;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateJpaAssociationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHibernateJpaAssociationApplication.class, args);
+    }
+
+}
